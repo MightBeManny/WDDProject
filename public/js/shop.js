27d36a6f20b509a0387d8e17535c9aa15c843803
Manny <S00255170@atu.ie>
2025-12-19 19:22:55 +0000

    Shop

---
+++ public/js/shop.js
@@ -0,0 +1,7 @@
+document.getElementById('addtocart').addEventListener('click', () => {
+
+    var total=localStorage.getItem('checkout');
+    total++;
+    localStorage.setItem('checkout',total);
+    document.querySelector('#checkout').innerHTML=total;
+})
